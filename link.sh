fc1ca19b866a348284d12c65cbca6eaa3040ac67
Henrique Bento <algarvio.henrique@gmail.com>
2021-03-16 14:22:21 +0000

    added shell script to symlink

---
+++ link.sh
@@ -0,0 +1,6 @@
+ln -s $PWD/.vimrc $HOME/.vimrc
+echo "linked vimrc"
+ln -s $PWD/.zshrc $HOME/.zshrc
+echo "linked zshrc"
+ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+echo "linked tmuxconf"
---
+++ unlink.sh
@@ -0,0 +1,6 @@
+rm $HOME/.vimrc
+echo "removed vimrc"
+rm $HOME/.zshrc
+echo "removed zshrc"
+rm $HOME/.tmux.conf
+echo "removed tmuxconf"
