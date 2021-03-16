db4f05168e9d4ce2dff6b9eab2875271cb9cec65
Henrique Bento <algarvio.henrique@gmail.com>
2021-03-16 18:56:17 +0000

    added remove files if present

--- link.sh
+++ link.sh
@@ -1,6 +1,17 @@
+delete_files() {
+	if [[ $1 ]]; then
+		rm $HOME/$1
+	fi
+}
+
+delete_files /.vimrc
 ln -s $PWD/.vimrc $HOME/.vimrc
 echo "linked vimrc"
+	
+delete_files /.zshrc
 ln -s $PWD/.zshrc $HOME/.zshrc
 echo "linked zshrc"
+
+delete_files /.tmux.conf
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
 echo "linked tmuxconf"
