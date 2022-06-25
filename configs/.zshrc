0138a67b37637cde4852f5d0903f5fb6dfe0f285
Henrique Bento <algarvio.henrique@gmail.com>
2022-06-25 18:50:31 +0100

    major refactor and coc-settings

--- configs/.zshrc
+++
@@ -1,3 +0,0 @@
-set -o vi
-alias ls="ls -G"
-prompt='%F{green}%m%f » %F{green}%15>...>%c%>>%f » '
--- install.sh
+++ install.sh
@@ -1,8 +1,58 @@
+unlink() {
+	if rm $HOME/$1; then
+		echo "Removed $1."
+	fi
+}
+
+link() {
+	if [[ -e $HOME/$2 ]]; then
+		rm $HOME/$2
+		echo "Removed already present file $2."
+	fi
+
+	ln -s $PWD/configs/$1 $HOME/$2
+
+	if [[ $? == 0 && -L $HOME/$2 ]]; then
+		echo "Linked $1."
+	fi
+}
+
+unlink_all() {
+	if [[ $OSTYPE == "darwin20" ]]; then
+		unlink .zshrc
+		unlink .vim/coc-settings.json
+	fi
+	unlink .bashrc
+	unlink .bash_profile
+	unlink .screenrc
+	unlink .vimrc
+}
+
+link_all() {
+	if [[ $OSTYPE == "darwin20" ]]; then
+		link zshrc .zshrc
+		link coc-settings.json .vim/coc-settings.json
+	fi
+	link bashrc .bashrc
+	link bash_profile .bash_profile
+	link screenrc .screenrc
+	link vimrc .vimrc
+}
+
+unlink_all
+link_all
+
+# check if it's first time setup and install required components
 which -s brew
 if [[ $OSTYPE == "darwin20" && $? != 0 ]]; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	brew install wget
-	brew install htop
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	brew install wget htop node bear go gopls fmt ripgrep rust rustfmt rustup-init tree cmake
+
+	# Run manually in vim:
+	# :PlugInstall
+	# :CocInstall coc-clangd
+	# :CocCommand clangd.install
+	# :CocInstall coc-rust-analyzer
 fi
-./unlink.sh
-./link.sh
+
